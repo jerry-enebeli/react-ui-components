8f112162a10fa57c8080ae7c086d145b9ba6d056
jerry <jerryenebeli@icloud.com>
2020-04-04 04:58:12 +0100

    use button components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { Button } from "./components/index";
+import "./styles/globals.scss";
 
 export default function App() {
   return (
     <div className="App">
-      <Button />
+      <h4>Buttons</h4>
+      <section className="container">
+        <Button> Default </Button>
+        <Button disabled> Disabled </Button>
+        <Button type="primary"> Primary </Button>
+        <Button type="danger"> Primary </Button>
+        <Button type="dashed"> Dashed </Button>
+        <Button type="dashed" shape="round">A</Button>
+        <Button type="primary" shape="round">CR</Button>
+      </section>
     </div>
   );
 }
