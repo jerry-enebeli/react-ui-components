3c3bfc8e598d9faf783858e3154a430789320fc0
jerry <jerryenebeli@icloud.com>
2020-04-04 04:58:27 +0100

    create button component

--- src/components/buttons/buttons.js
+++ src/components/buttons/buttons.js
@@ -1,8 +1,36 @@
 import React from "react";
 import "./style.scss";
 
-const Button = props => {
-  return <button className="button">Click</button>;
+const Button = (props) => {
+  let buttonType = "";
+
+  switch (props.type) {
+    case "primary":
+      buttonType = "btn--primary";
+      break;
+    case "danger":
+      buttonType = "btn--danger";
+      break;
+    case "dashed":
+      buttonType = "btn--dashed";
+      break;
+    default:
+      buttonType = "btn";
+      break;
+  }
+
+  switch (props.shape) {
+    case "round":
+      buttonType = `${buttonType} btn--circle`;
+      break;
+    default:
+      break;
+  }
+
+  console.log(props)
+  return (
+      <button className={buttonType}  {...props} type="button">{props.children}</button>
+  );
 };
 
 export default Button;
