31e869c810c421b0df98ffad9376b5672d205773
jerry <jerryenebeli@icloud.com>
2020-04-05 23:04:33 +0100

    import breadcrumb component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import button from "./buttons/buttons";
+import breadCrumb from "./bread-crumb/bread_crumb";
 
 //export all components
 export const Button = button;
+export const Breadcrumb =  breadCrumb
