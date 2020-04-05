27518e0782779772e165d2d16b0b7e2e0e51c301
jerry <jerryenebeli@icloud.com>
2020-04-05 23:04:20 +0100

    apply breadcrumb component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from "react";
-import { Button } from "./components/index";
+import { Button, Breadcrumb } from "./components/index";
 import "./styles/globals.scss";
 
 export default function App() {
   return (
     <div className="App">
-      <h4>Buttons</h4>
-      <section className="container">
-        <Button> Default </Button>
-        <Button disabled> Disabled </Button>
-        <Button type="primary"> Primary </Button>
-        <Button type="danger"> Danger </Button>
-        <Button type="dashed"> Dashed </Button>
-        <Button type="dashed" shape="round">A</Button>
-        <Button type="primary" shape="round">CR</Button>
-      </section>
+      <div>
+        <h4>Buttons</h4>
+        <section className="container">
+          <Button> Default </Button>
+          <Button disabled> Disabled </Button>
+          <Button type="primary"> Primary </Button>
+          <Button type="danger"> Danger </Button>
+          <Button type="dashed"> Dashed </Button>
+          <Button type="dashed" shape="round">
+            A
+          </Button>
+          <Button type="primary" shape="round">
+            CR
+          </Button>
+        </section>
+      </div>
+
+      <div>
+        <h4>BreadCrumb</h4>
+        <section className="container">
+          <Breadcrumb.Breadcrumb>
+            <Breadcrumb.Items name="Home" link="#"></Breadcrumb.Items>
+            <Breadcrumb.Items name="User" link="#"></Breadcrumb.Items>
+            <Breadcrumb.Items name="Profile" link="#"></Breadcrumb.Items>
+          </Breadcrumb.Breadcrumb>
+
+          <Breadcrumb.Breadcrumb separator=">">
+            <Breadcrumb.Items name="Home" link="#"></Breadcrumb.Items>
+            <Breadcrumb.Items name="User" link="#"></Breadcrumb.Items>
+            <Breadcrumb.Items name="Profile" link="#"></Breadcrumb.Items>
+          </Breadcrumb.Breadcrumb>
+        </section>
+      </div>
     </div>
   );
 }
